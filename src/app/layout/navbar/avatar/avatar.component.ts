ba53503bf176cd22214342750f378d32db696912
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-04-02 07:19:45 +0200

    Avatar component

--- src/app/layout/navbar/avatar/avatar.component.ts
+++ src/app/layout/navbar/avatar/avatar.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, input, InputSignal } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-avatar',
   standalone: true,
-  imports: [],
+  imports: [NgClass, FontAwesomeModule],
   templateUrl: './avatar.component.html',
   styleUrl: './avatar.component.scss'
 })
 export class AvatarComponent {
+
+  imageUrl: InputSignal<string | undefined> = input<string>();
+  avatarSize:  InputSignal<"avatar-sm" | "avatar-xl" | undefined> = input<"avatar-sm" | "avatar-xl">();
 
 }
