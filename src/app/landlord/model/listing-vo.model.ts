bea9401843095c8d78e324c00128c462748373f3
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-06-04 07:49:20 +0200

    Landlord model

---
+++ src/app/landlord/model/listing-vo.model.ts
@@ -0,0 +1,27 @@
+export interface GuestVO {
+    value: number
+}
+
+export interface BedroomsVO {
+    value: number
+}
+
+export interface BedsVO {
+    value: number
+}
+
+export interface BathsVO {
+    value: number
+}
+
+export interface TitleVO {
+    value: number
+}
+
+export interface DescriptionVO {
+    value: number
+}
+
+export interface priceVO {
+    value: number
+}
---
+++ src/app/landlord/model/listing.model.ts
@@ -0,0 +1,28 @@
+import { CategoryName } from "../../layout/navbar/category/category.model";
+import { BathsVO, BedroomsVO, BedsVO, DescriptionVO, GuestVO, priceVO, TitleVO } from "./listing-vo.model";
+import { NewListingPicture } from "./picture.model";
+
+export interface NewListingInfo {
+    guests: GuestVO,
+    bedrooms: BedroomsVO,
+    beds: BedsVO,
+    baths: BathsVO
+}
+
+export interface NewLising {
+    category: CategoryName,
+    location: string,
+    infos: NewListingInfo,
+    picture: Array<NewListingPicture>,
+    description: Description,
+    price: priceVO
+}
+
+export interface Description {
+    title: TitleVO,
+    description: DescriptionVO
+}
+
+export interface CreatedListing {
+    publicId: string
+}
